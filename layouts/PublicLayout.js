e65e869800f06cac3f00c875280910a75e54f9f5
websterhf18 <websterh18@gmail.com>
2021-07-10 21:48:09 -0500

    Fix

--- layouts/PublicLayout.js
+++ layouts/PublicLayout.js
@@ -16,7 +16,7 @@ const PublicLayout = (props) => {
                 <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" media="print" onload="this.media='all';this.onload=null;"/>
                 <link media="all" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
                 <link media="all" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-                <link href="/css/app.min.css?v=2.11" rel="stylesheet" media="print" onload="this.media='all';this.onload=null;" />
+                <link href="/css/app.min.css?v=2.11" rel="preload" as="style" media="print" onload="this.media='all';this.onload=null;" />
                 <script media="all" defer src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
                 <script media="all" defer src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
                 <script media="all" defer src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
