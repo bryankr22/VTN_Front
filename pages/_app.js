e8e4c7102b3ad62820d30dd2742ba40eec8f9799
websterhf18 <websterh18@gmail.com>
2021-07-11 09:36:50 -0500

    Remove slick

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,10 @@
 import 'semantic-ui-css/semantic.min.css'
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import '../public/css/app.min.css'
 import '../styles/globals.css'
 import { Provider } from 'react-redux'
 import store from '../store';
-import React from 'react';
+
 function MyApp({ Component, pageProps }) {
-    React.useEffect(() => {
-        // Remove the server-side injected CSS.
-        const jssStyles = document.querySelector('#jss-server-side');
-        if (jssStyles) {
-            jssStyles.parentElement.removeChild(jssStyles);
-        }
-    }, []);
     return (
         <Provider store={store}>
             <Component {...pageProps} />
