036f3e4f2af4a4dd08ea6174bc11abbc6db790c4
websterhf18 <websterh18@gmail.com>
2021-07-11 09:24:50 -0500

    Update css

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,7 @@
+import 'semantic-ui-css/semantic.min.css'
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import '../public/css/app.min.css'
 import '../styles/globals.css'
 import { Provider } from 'react-redux'
 import store from '../store';
