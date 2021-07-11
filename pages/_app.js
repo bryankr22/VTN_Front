7f7318b74ca5a8520f7142f26b8aa7aa24703696
websterhf18 <websterh18@gmail.com>
2021-07-11 09:48:02 -0500

    Test points

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../public/css/app.min.css'
+//import 'semantic-ui-css/semantic.min.css'
+//import '../public/css/app.min.css'
 import '../styles/globals.css'
 import { Provider } from 'react-redux'
 import store from '../store';
