55b69f7a7c0aab8f51382f8b892bcf23348db529
websterhf18 <websterh18@gmail.com>
2021-07-14 00:55:44 -0500

    Update css

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
 const withPWA = require('next-pwa');
 const withCss = require("@zeit/next-css");
 const withPurgeCss = require("next-purgecss");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
 module.exports = withCss(
     withPurgeCss({
+        webpack(config, options) {
+            config.optimization.minimizer = [];
+            config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
+            return config;
+        },
         purgeCssEnabled: ({ dev, isServer }) => !dev && !isServer, // Only enable PurgeCSS for client-side production builds
+        purgeCssPaths: [
+            "pages/**/*",
+            "components/**/*",
+            "public/css/*"
+        ],
     })
 );
 module.exports = withPWA({
