d78d5aabbfbd9ca34e0fdfd993b789a09ebabc2e
websterhf18 <websterh18@gmail.com>
2021-07-14 00:44:00 -0500

    Update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 const withPWA = require('next-pwa');
 const withCss = require("@zeit/next-css");
 const withPurgeCss = require("next-purgecss");
-module.exports = withCss(withPurgeCss());
+module.exports = withCss(
+    withPurgeCss({
+        purgeCssEnabled: ({ dev, isServer }) => !dev && !isServer, // Only enable PurgeCSS for client-side production builds
+    })
+);
 module.exports = withPWA({
   pwa: {
     dest: 'public'
