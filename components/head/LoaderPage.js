994b4f1f276bef4645586e84ad29aef2123c95d7
websterhf18 <websterh18@gmail.com>
2021-08-11 08:13:18 -0500

    Update page loader

--- components/head/LoaderPage.js
+++ components/head/LoaderPage.js
@@ -5,19 +5,21 @@ export default function Loading() {
     const router = useRouter();
     const [loading, setLoading] = useState(false);
     useEffect(() => {
-        const handleStart = (url) => (url !== router.asPath) && setLoading(true);
-        const handleComplete = (url) => (url === router.asPath) && setLoading(false);
+        const handleStart = (url) => setLoading(true);
+        const handleComplete = (url) => setLoading(false);
 
         router.events.on('routeChangeStart', handleStart)
         router.events.on('routeChangeComplete', handleComplete)
         router.events.on('routeChangeError', handleComplete)
+        //router.events.on('hashChangeComplete', handleComplete)
+        
 
         return () => {
             router.events.off('routeChangeStart', handleStart)
             router.events.off('routeChangeComplete', handleComplete)
             router.events.off('routeChangeError', handleComplete)
         }
-    })
+    },[])
     return loading && (
         <Dimmer style={{ position: "fixed", zIndex: 9999 }} active={loading}>
             <Loader>Cargando pagina...</Loader>
