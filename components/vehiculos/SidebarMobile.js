075cc0282d7520ebe220e1f716365ac8db91c261
Camilo Ortiz <9ortizcamilo@gmail.com>
2021-09-17 00:15:00 -0500

    Filters mobile

--- components/vehiculos/SidebarMobile.js
+++ components/vehiculos/SidebarMobile.js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Input, Header, Button, Container } from "semantic-ui-react";
 import ActiveTagsVehiculos from "./ActiveTagsVehiculos";
 import ModalFiltersMobile from "./modals/ModalFiltersMobile";
@@ -20,13 +20,35 @@ export default function SidebarMobile({
       handleSubmit();
     }
   };
-  const handleSubmit = () => {
-    if (!isFicha) {
-      router.push("/vehiculos?q=" + query);
-    } else {
-      router.push("/ficha-tecnica?q=" + query);
+
+  const insertParam = (key, value) => {
+    key = encodeURIComponent(key);
+    value = encodeURIComponent(value);
+    var kvp = document.location.search.substr(1).split('&');
+    let i=0;
+    for(; i<kvp.length; i++){
+        if (kvp[i].startsWith(key + '=')) {
+            let pair = kvp[i].split('=');
+            pair[1] = value;
+            kvp[i] = pair.join('=');
+            break;
+        }
     }
+    if(i >= kvp.length){
+        kvp[kvp.length] = [key,value].join('=');
+    }
+    let params = kvp.join('&');
+    document.location.search = params;
+}
+
+  const handleSubmit = () => {
+    insertParam('q', query)
   };
+
+  useEffect(() => {
+    setQuery(params.q)
+  }, [])
+
   return (
     <>
       <Input
