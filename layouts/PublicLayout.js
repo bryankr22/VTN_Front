34ef723c3d0445b137aac47d58726ae60cdbd57e
Santiago Arias <32374120+SOY4RIAS@users.noreply.github.com>
2021-10-01 05:03:02 +0000

    add SEO meta tags

--- layouts/PublicLayout.js
+++ layouts/PublicLayout.js
@@ -3,13 +3,33 @@ import React from 'react';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
 import LoaderPage from '../components/head/LoaderPage';
+import { DefaultSeo } from 'next-seo';
 const PublicLayout = (props) => {
     return(
         <>
             <Head>
                 <meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, width=device-width" />
-                <title>VendeTuNave - Carros en Venta</title>
             </Head>
+            <DefaultSeo
+            title="VendeTuNave - Carros en Venta"
+            defaultOpenGraphImageHeight={200}
+            defaultOpenGraphImageWidth={150}
+            description="Vende tu vehículo completamente gratis en VendeTuNave. Encuentra carros, camionetas, motos, servicios y recomendaciones en un solo sitio."
+            openGraph={{
+              type: "website",
+              locale: "es_ES",
+              url: "https://vendetunave.co",
+              site_name: "VendeTuNave",
+              title: 'VendeTuNave',
+              images: [
+                  {
+                      url: 'https://vendetunave.co/images/logo_VTN.png',
+                      height: 200,
+                      width: 150
+                  }
+              ]
+            }}
+          /> 
             
             <div className="container">
                 <Header {...props} />
--- pages/_document.js
+++ pages/_document.js
@@ -1,53 +1,83 @@
-import Document, { Html, Main, Head, NextScript } from 'next/document'
-import { getLangFromReq } from '../helpers/fromReq'
+import { DefaultSeo } from "next-seo";
+import Document, { Html, Main, Head, NextScript } from "next/document";
+import { getLangFromReq } from "../helpers/fromReq";
 
 class MyDocument extends Document {
-    static async getInitialProps(ctx) {
-        const initialProps = await Document.getInitialProps(ctx)
-        const lang = getLangFromReq(ctx.req)
-        return { ...initialProps, lang }
-    }
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    const lang = getLangFromReq(ctx.req);
+    return { ...initialProps, lang };
+  }
 
-    render() {
-        return (
-            <Html lang={this.props.lang}>
-                <Head>
-                    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-                    <meta name="Description" content="Vende tu vehículo completamente gratis en VendeTuNave. Encuentra carros, camionetas, motos, servicios y recomendaciones en un solo sitio."></meta>
-                    <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link media="all" href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css" />
-                    <link media="all" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" rel="stylesheet" type="text/css" />
-                    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-                    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
-                    <script src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js"></script>
-                    <script src="https://cdn.jsdelivr.net/npm/ez-plus-extended@1.0.2/src/jquery.ez-plus.min.js"></script>
-                    <meta name="description" content="Vende tu vehículo completamente gratis en VendeTuNave. Encuentra carros, camionetas, motos, servicios y recomendaciones en un solo sitio." />
-                    <meta name="keywords" content="vende tu nave, carros en venta, carros de segunda, mercado libre carros, venta de carros usados y nuevos, compra y venta de carros, compra y venta motos, venta de carros" />
-                    {process?.env?.NODE_ENV != 'development' && (
-                        <>
-                        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-158787289-1"></script>
-                        <script dangerouslySetInnerHTML={{
-                        __html: `
+  render() {
+    return (
+      <Html lang={this.props.lang}>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            media="all"
+            href="https://fonts.googleapis.com/css?family=Raleway:100,600"
+            rel="stylesheet"
+            type="text/css"
+          />
+          <link
+            media="all"
+            href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
+            rel="stylesheet"
+            type="text/css"
+          />
+          <meta
+            name="keywords"
+            content="vende tu nave, carros en venta, carros de segunda, mercado libre carros, venta de carros usados y nuevos, compra y venta de carros, compra y venta motos, venta de carros"
+          />
+          <meta name="robots" content="index, follow" />
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name="language" content="Spanish" />
+          <meta name="revisit-after" content="1 days" />
+          <meta name="author" content="VTN - VendeTuNave" />
+          <meta httpEquiv="cache-control" content="no-cache" />
+          <meta name="Rating" content="General" />
+          <meta name="Language" content="es" />
+          <meta name="distribution" content="Global" />
+          <meta name="Copyright" content="VendeTuNave" />
+          <meta name="Classification" content="" />
+          <meta name="zipcode" content="760001" />
+          <meta name="city" content="Cali" />
+          <meta name="country" content="Colombia" />
+
+          <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+          <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js"></script>
+          <script src="https://cdn.jsdelivr.net/npm/ez-plus-extended@1.0.2/src/jquery.ez-plus.min.js"></script>
+          {process?.env?.NODE_ENV != "development" && (
+            <>
+              <script
+                async
+                src="https://www.googletagmanager.com/gtag/js?id=UA-158787289-1"
+              ></script>
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
                             window.dataLayer = window.dataLayer || [];
                             function gtag(){dataLayer.push(arguments);}
                             gtag('js', new Date());
 
                             gtag('config', 'UA-158787289-1');
-                        `
-                        }}>
-                        </script>
-                        </>
-                    )}
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+                        `,
+                }}
+              ></script>
+            </>
+          )}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
-export default MyDocument
+export default MyDocument;
