eddb71a9de377b158d01ff7d53634bd090295307
Santiago Arias <32374120+SOY4RIAS@users.noreply.github.com>
2021-12-11 04:11:00 +0000

    show indicator bar always

--- components/VehicleThumbnail/index.tsx
+++ components/VehicleThumbnail/index.tsx
@@ -1,21 +1,7 @@
 import { Icon, Image } from "semantic-ui-react";
 import styles from "./styles.module.css";
 
-const showSection = ({
-  permuta,
-  financiacion,
-  confiable,
-  blindado,
-  peritaje,
-}) => {
-  return (
-    [permuta, financiacion, confiable, blindado].some(Boolean) ||
-    peritaje != "0"
-  );
-};
-
 export default function VehicleThumbnail({ item, src, compact }) {
-  console.log(item, " >>>>>>");
   return (
     <div className={styles.imageWrapped}>
       <Image
@@ -34,19 +20,15 @@ export default function VehicleThumbnail({ item, src, compact }) {
           }
         }
       />
-      {showSection(item) && (
-        <div className={styles.indicator}>
-          {item.permuta == 1 && <Icon name="exchange" title="permuta" />}
-          {item.financiacion == 1 && (
-            <Icon name="wpforms" title="financiación" />
-          )}
-          {item.confiable == 1 && <Icon name="check" title="confiable" />}
-          {item.blindado == 1 && <Icon name="shield" title="blindado" />}
-          {item.peritaje != "0" && (
-            <Icon name="clipboard check" title="peritaje" />
-          )}
-        </div>
-      )}
+      <div className={styles.indicator}>
+        {item.permuta == 1 && <Icon name="exchange" title="permuta" />}
+        {item.financiacion == 1 && <Icon name="wpforms" title="financiación" />}
+        {item.confiable == 1 && <Icon name="check" title="confiable" />}
+        {item.blindado == 1 && <Icon name="shield" title="blindado" />}
+        {item.peritaje != "0" && (
+          <Icon name="clipboard check" title="peritaje" />
+        )}
+      </div>
     </div>
   );
 }
